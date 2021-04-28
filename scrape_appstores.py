f0efc4fe1c0f28eecd00b7c0fab8db72f8c6f9ec
BuildTools <maedhoff@ucsc.edu>
2021-04-28 17:02:33 +0200

    added itunes scraping to scrape_appstores.py

--- scrape_appstores.py
+++ scrape_appstores.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 import re
 import os
+import unicodedata
 from bs4 import BeautifulSoup
 from datetime import datetime
 
@@ -29,51 +30,59 @@ urls = {
 }
 
 def extract_from_soup(soup, url):
-    app_fullname = NA
-    app_id = NA
-    app_size = NA
-    app_version = NA
-    last_updated = NA
-    n_ratings = NA
-    downloads = NA
-    avg_rating = NA
-    platform = NA
+    app_data = {
+        'app_fullname': NA,
+        'app_name': NA,
+        'app_id': NA,
+        'platform': NA,
+        'scraping_timestamp': datetime.now().replace(microsecond=0).isoformat(' '), # aktuelle Uhrzeit formatiert nach ISO 861
+        'app_size': NA,
+        'version': NA,
+        'last_updated': NA,
+        'downloads_bin': NA,
+        'n_ratings': NA,
+        'average_rating': NA
+    }
 
     if 'play.google.com' in url:
-        platform = 'google_play'
-
-        app_fullname = soup.select('h1 span')[0].text
-        app_id = url.split("?id=")[-1].split('&')[0] # letzter Bestandteil der URL
+        app_data['platform'] = 'google_play'
 
         try:
-            app_size = list(soup.find_all(text=re.compile('Size'))[0].parent.next_siblings)[-1].text
-            app_version = list(soup.find_all(text=re.compile('Current Version'))[0].parent.next_siblings)[0].text
-            last_updated = list(soup.find_all(text=re.compile('Updated'))[0].parent.next_siblings)[0].text
-            downloads = list(soup.find_all(text=re.compile('Installs'))[0].parent.next_siblings)[0].text
-            n_ratings = [int(span.text) for span in soup.select('c-wiz span span') if span.text.isdigit()][0]
-            avg_rating = [div['aria-label'] for div in soup.select('div') if div.has_attr('aria-label') and "stars out of" in div['aria-label']][0].split()[1]
+            app_data['app_fullname'] = soup.select('h1 span')[0].text
+            app_data['app_id'] = url.split("?id=")[-1].split('&')[0] # letzter Bestandteil der URL
+            app_data['app_size'] = list(soup.find_all(text=re.compile('Size'))[0].parent.next_siblings)[-1].text
+            app_data['version'] = list(soup.find_all(text=re.compile('Current Version'))[0].parent.next_siblings)[0].text
+            app_data['last_updated'] = list(soup.find_all(text=re.compile('Updated'))[0].parent.next_siblings)[0].text
+            app_data['downloads_bin'] = list(soup.find_all(text=re.compile('Installs'))[0].parent.next_siblings)[0].text
+            app_data['n_ratings'] = [int(span.text) for span in soup.select('c-wiz span span') if span.text.isdigit()][0]
+            app_data['average_rating'] = [div['aria-label'] for div in soup.select('div') if div.has_attr('aria-label') and "stars out of" in div['aria-label']][0].split()[1]
 
         except Exception as e:
             print(f"Error parsing {url}: {e}")
 
     elif 'apps.apple.com' in url:
-        platform = 'itunes'
+        app_data['platform'] = 'itunes'
 
-        app_id = url.split("/")[-1][2:]
+        try:
+            app_data['app_fullname'] = soup.select('.product-header__title')[0].find(text=True, recursive=False).strip()
+            app_data['app_id'] = url.split("/")[-1][2:]
+            app_data['app_size'] = unicodedata.normalize('NFKD', soup.select('.information-list--app > .information-list__item:nth-child(2) > .information-list__item__definition')[0].text)
+            app_data['version'] = soup.select('.whats-new__latest__version')[0].text.split()[-1]
+            app_data['last_updated'] = [t for t in soup.select('time') if t.has_attr('data-test-we-datetime')][0].text
+            app_data['n_ratings'] = soup.select('.we-customer-ratings__count')[0].text.split()[0]
+            app_data['average_rating'] = soup.select('.we-customer-ratings__averages__display')[0].text
 
-    return {
-        'app_name': re.findall(r"[\w']+|$", app_fullname)[0], # first word
-        'app_fullname': app_fullname,
-        'app_id': app_id,
-        'platform': platform,
-        'scraping_timestamp': datetime.now().replace(microsecond=0).isoformat(' '), # aktuelle Uhrzeit formatiert nach ISO 861
-        'app_size': app_size,
-        'version': app_version,
-        'last_updated': last_updated,
-        'downloads_bin': downloads,
-        'n_ratings': n_ratings,
-        'average_rating': avg_rating
-    }
+        except Exception as e:
+            print(f"Error parsing {url}: {e}")
+    
+    """ app_name should be the first word. split at whitespace. sometimes, the first
+    word ends with a ':' or similar, so remove the last character if it's not a letter."""
+
+    app_data['app_name'] = app_data['app_fullname'].split()[0] # first word of the long name
+    if not app_data['app_name'][-1].isalpha():
+        app_data['app_name'] = app_data['app_name'][:-1]
+
+    return app_data
 
 rows = []
 for url in [url for store in urls for url in urls[store]]:
@@ -82,9 +91,10 @@ for url in [url for store in urls for url in urls[store]]:
 
     rows.append(extract_from_soup(soup, url))
 
-out_path = 'app_results.csv'
+out_path = 'appstore_data.csv'
 df = pd.DataFrame(rows)
 df.to_csv(out_path, 
     index = False,
     mode = 'a',
-    header = not os.path.exists(out_path)) # add header only if file didn't exist before
+    #header = not os.path.exists(out_path)) # add header only if file didn't exist before
+)
